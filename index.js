8107b5958a739a1fd51c9a4c6ca1e34b2acded56
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-02-18 19:54:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return("I love you, too.")
-  } else if
+  } else if () {
+    return
+  } else {
+    return 
+  }
 }
